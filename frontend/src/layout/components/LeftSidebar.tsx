4c3ce379a68a9f5c14748b1383b147bac2ba14ec
Headliner770 <paul248022@gmail.com>
2025-06-08 17:42:42 +0300

    songs

--- frontend/src/layout/components/LeftSidebar.tsx
+++ frontend/src/layout/components/LeftSidebar.tsx
@@ -1,10 +1,14 @@
 import { buttonVariants } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
 import { SignedIn } from "@clerk/clerk-react";
-import { HomeIcon, MessageCircle } from "lucide-react";
+import { ScrollArea } from "@radix-ui/react-scroll-area";
+import { HomeIcon, Library, MessageCircle } from "lucide-react";
 import { Link } from "react-router-dom";
+import PlaylistSkeleton from "./skeletons/PlaylistSkeleton";
 
 const LeftSidebar = () => {
+  const isLoading = false;
+  
   return (
     <div className="h-full flex-col gap-2">
       {/* Navigation menu */}
@@ -26,7 +30,7 @@ const LeftSidebar = () => {
 
           <SignedIn>
             <Link
-              to={"/"}
+              to={"/chat"}
               className={cn(
                 buttonVariants({
                   variant: "ghost",
@@ -43,6 +47,21 @@ const LeftSidebar = () => {
       </div>
 
       {/* Library section */}
+
+      <div className="flex-1 rounded-lg bg-zinc-900 p-4">
+        <div className="flex items-center justify-between mb-4">
+          <div className="flex items-center text-white px-2">
+            <Library className="size-5 mr-2" />
+            <span className="hidden md:inline">Playlists</span>
+          </div>
+        </div>
+
+        <ScrollArea className="h-[calc(100vh-300px)]">
+          <div className="space-y-2">
+            {isLoading ? <PlaylistSkeleton /> : "some music"}
+          </div>
+        </ScrollArea>
+      </div>
     </div>
   );
 };
